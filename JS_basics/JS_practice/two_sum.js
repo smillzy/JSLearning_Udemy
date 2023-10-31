aafcaf289db369dda3dd960114b26c0ad8fde121
Phoebe Lee <132736977+smillzy@users.noreply.github.com>
2023-10-31 21:25:44 +0800

    update wrong place

--- JS_basics/JS_practice/two_sum.js
+++ JS_basics/JS_practice/two_sum.js
@@ -1,11 +1,13 @@
-var twoSum = function(nums, target) {
-
-    for(var i = 0 ; i < nums.length ; i++){
-        for(var j = i+1 ; j < nums.length ; j++ ){
-            if(  nums[i] + nums[j]  == target ){
-                return [i,j];
-            }
-        }
-
+function twosum(nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        console.log([i, j]);
+      }
     }
-};
+  }
+}
+
+let nums = [2, 7, 11, 15];
+let target = 9;
+twosum(nums, target);
